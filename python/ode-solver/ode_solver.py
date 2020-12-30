96f06eb25ae1ae4a103d4e6c3359d6a44f6bb47b
Sergey Royz <sergey@ext.latch.com>
2020-12-30 17:09:30 +0100

    merge support

--- python/ode-solver/ode_solver.py
+++ python/ode-solver/ode_solver.py
@@ -1,41 +1,42 @@
 import numpy as np
 
 
-def integrate_heuns(state_, t_, dt_, dydx_func):
-    k1 = dydx_func(state_, t_, dt_)
-    k2 = dydx_func([v + d * dt_ for v, d in zip(state_, k1)], t_, dt_)
-    return [v + (k1_ + k2_) * dt_ / 2 for v, k1_, k2_ in zip(state_, k1, k2)]
+def integrate_heuns(state, step, t, dt, dydx_func):
+    k1 = dydx_func(state, step, t, dt)
+    k2 = dydx_func([v + d * dt for v, d in zip(state, k1)], step, t, dt)
+    return [v + (k1_ + k2_) * dt / 2 for v, k1_, k2_ in zip(state, k1, k2)]
 
 
-def integrate_euler(state_, t_, dt_, dydx_func):
-    k1 = dydx_func(state_, t_, dt_)
-    return [v + k1_ * dt_ for v, k1_ in zip(state_, k1)]
+def integrate_euler(state, step, t, dt, dydx_func):
+    k1 = dydx_func(state, step, t, dt)
+    return [v + k1_ * dt for v, k1_ in zip(state, k1)]
 
 
-def integrate_rk4(state_, t_, dt_, dydx_func):
+def integrate_rk4(state, step, t, dt, dydx_func):
     """
     Source: https://www.geeksforgeeks.org/runge-kutta-4th-order-method-solve-differential-equation/
-    :param state_:
-    :param t_:
-    :param dt_:
+    :param step:
+    :param state:
+    :param t:
+    :param dt:
     :param dydx_func:
     :return:
     """
-    k1 = dydx_func(state_, t_, dt_)
-    k2 = dydx_func([v + d * dt_ / 2 for v, d in zip(state_, k1)], t_, dt_)
-    k3 = dydx_func([v + d * dt_ / 2 for v, d in zip(state_, k2)], t_, dt_)
-    k4 = dydx_func([v + d * dt_ for v, d in zip(state_, k3)], t_, dt_)
-    return [v + (k1_ + 2 * k2_ + 2 * k3_ + k4_) * dt_ / 6 for v, k1_, k2_, k3_, k4_ in zip(state_, k1, k2, k3, k4)]
+    k1 = dydx_func(state, step, t, dt)
+    k2 = dydx_func([v + d * dt / 2 for v, d in zip(state, k1)], step, t, dt)
+    k3 = dydx_func([v + d * dt / 2 for v, d in zip(state, k2)], step, t, dt)
+    k4 = dydx_func([v + d * dt for v, d in zip(state, k3)], step, t, dt)
+    return [v + (k1_ + 2 * k2_ + 2 * k3_ + k4_) * dt / 6 for v, k1_, k2_, k3_, k4_ in zip(state, k1, k2, k3, k4)]
 
 
-def derivatives_circle(state_, t_, dt_):
-    x_, v_ = state_
-    return [v_, -x_]
+def derivatives_circle(state, step, t, dt):
+    x, v = state
+    return [v, -x]
 
 
-def solve(initial_state_, times_, integrate_func, derivative_func):
-    dt = times_[1] - times_[0]
-    states_ = [initial_state_]
-    for t in times_:
-        states_.append(integrate_func(states_[-1], t, dt, derivative_func))
+def solve(initial_state, times, integrate_func, derivative_func):
+    dt = times[1] - times[0]
+    states_ = [initial_state]
+    for step, t in enumerate(times):
+        states_.append(integrate_func(states_[-1], step, t, dt, derivative_func))
     return np.array(states_)
--- python/ode-solver/three_body_gravity_sim.py
+++ python/ode-solver/three_body_gravity_sim.py
@@ -7,6 +7,7 @@ from matplotlib import animation
 from ode_solver import solve, integrate_rk4
 
 G = 0.5
+rho = 100.0
 
 times = np.linspace(0, 2.5, 1000)
 
@@ -22,14 +23,16 @@ velocities = [
     [0.0, 0.0]
 ]
 
-masses = [50, 50, 4.0]
+masses = np.array([50, 50, 4.0])
+radiuses = (masses * 0.75 / np.pi / rho) ** (1 / 3)
+merge_step = {}
 
 
 def to_state(positions_, velocities_):
     return list(reduce(lambda x, y: x + y, map(lambda x: x[0] + x[1], zip(positions_, velocities_)), []))
 
 
-def derivatives_gravity_n_bodies(state_, t_, dt_):
+def derivatives_gravity_n_bodies(state_, step, t_, dt_):
     xs = state_[::4]
     ys = state_[1::4]
     vxs = state_[2::4]
@@ -41,11 +44,27 @@ def derivatives_gravity_n_bodies(state_, t_, dt_):
         for j in range(len(xs)):
             if i == j:
                 continue
+
+            m_i, m_j = masses[i], masses[j]
+
+            if m_i == 0 or m_j == 0:
+                continue
+
             dist2 = (xs[i] - xs[j]) ** 2 + (ys[i] - ys[j]) ** 2
-            a = G * masses[i] * masses[j] / dist2
             dist = np.sqrt(dist2)
-            axs[i] += a * (xs[j] - xs[i]) / dist
-            ays[i] += a * (ys[j] - ys[i]) / dist
+
+            if dist <= radiuses[i] + radiuses[j]:
+                vxs[i] = m_i * vxs[i] + m_j * vxs[j] / (m_i + m_j)
+                vys[i] = m_i * vys[i] + m_j * vys[j] / (m_i + m_j)
+
+                masses[i] = m_i + m_j
+                masses[j] = 0
+                merge_step[j] = step
+            else:
+                a = G * masses[i] * masses[j] / dist2
+
+                axs[i] += a * (xs[j] - xs[i]) / dist
+                ays[i] += a * (ys[j] - ys[i]) / dist
 
     positions_ = [list(x) for x in zip(vxs, vys)]
     velocities_ = [list(x) for x in zip(axs, ays)]
@@ -61,7 +80,11 @@ def render(solution_):
     objects = []
     print(xs.shape)
     for i in range(xs.shape[1]):
-        pp.plot(xs[:, i], ys[:, i], label=f"#{i}", linewidth=0.5)
+        x, y = xs[:, i], ys[:, i]
+        if i in merge_step:
+            x = x[:merge_step[i]]
+            y = y[:merge_step[i]]
+        pp.plot(x, y, label=f"#{i}", linewidth=0.5)
         obj, = pp.plot([], [], marker='o', markersize=10)
         obj.set_data([], [])
         objects.append(obj)
@@ -71,7 +94,10 @@ def render(solution_):
 
     def animate(i_):
         for j in range(len(objects)):
-            objects[j].set_data(xs[i_, j], ys[i_, j])
+            if j in merge_step and i_ >= merge_step[j]:
+                objects[j].set_data([], [])
+            else:
+                objects[j].set_data(xs[i_, j], ys[i_, j])
         return objects
 
     ani = animation.FuncAnimation(fig, animate,
