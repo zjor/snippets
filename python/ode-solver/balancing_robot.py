5122ca4a3cd9fc39024d0398e5d6521d5caeff5b
Sergey Royz <zjor.se@gmail.com>
2021-03-30 23:41:48 +0200

    balancing robot: free fall

---
+++ python/ode-solver/balancing_robot.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+from numpy import sin, cos, pi
+from ode_solver import solve, integrate_rk4
+
+r = 0.25
+l = 1.0
+M = 0.25
+m = 0.3
+g = 9.8
+
+I = 0.5 * M * r
+
+
+def derivate(state, step, t, dt):
+    dth, th, dphi, phi = state
+
+    _dphi = (m * l * r * dth ** 2 * sin(th) - m * g * r * sin(th) * cos(th)) / (m * r ** 2 * sin(th) ** 2 + I)
+    _dth = (g * sin(th) - r * _dphi * cos(th)) / l
+
+    return [_dth, dth, _dphi, dphi]
+
+
+if __name__ == "__main__":
+    times = np.linspace(0, 10, 500)
+    solution = solve([0.0, pi / 12, .0, .0], times, integrate_rk4, derivate)
+    theta = solution[:, 1]
+    phi = solution[:, 3]
+
+    wheel_x = phi * r
+
+    spot_r = 0.7 * r
+    wheel_spot_x = wheel_x + spot_r * cos(phi - pi / 2)
+    wheel_spot_y = r - spot_r * sin(phi - pi / 2)
+
+    mass_x = wheel_x + l * cos(theta - pi / 2)
+    mass_y = r - l * sin(theta - pi / 2)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, autoscale_on=False, xlim=(-1.5, 1.5), ylim=(-1.5, 1.5))
+    ax.set_aspect('equal')
+    ax.grid()
+
+    line, = ax.plot([], [], 'k-', lw=2)
+    wheel = plt.Circle((0.0, r), r, color='black', fill=False, lw=2)
+    wheel_spot = plt.Circle((0.0, spot_r), 0.02, color='red')
+    mass = plt.Circle((0.0, 0.0), 0.1, color='black')
+
+
+    def init():
+        return []
+
+
+    def animate(i):
+        wheel.set_center((wheel_x[i], r))
+        wheel_spot.set_center((wheel_spot_x[i], wheel_spot_y[i]))
+        mass.set_center((mass_x[i], mass_y[i]))
+        line.set_data([wheel_x[i], mass_x[i]], [r, mass_y[i]])
+        patches = [line, ax.add_patch(wheel), ax.add_patch(wheel_spot), ax.add_patch(mass)]
+        return patches
+
+
+    ani = animation.FuncAnimation(fig, animate, np.arange(1, len(solution)),
+                                  interval=25, blit=True, init_func=init)
+
+    plt.show()
+
+    # ani.save(f"{__file__[:-3]}.gif", writer='imagemagick', fps=24)
+    # TODO: add def func(current_frame: int, total_frames: int) -> Any
--- python/ode-solver/mass_on_wheel.py
+++ python/ode-solver/mass_on_wheel.py
@@ -2,11 +2,11 @@
 A mass (m) is attached to a weightless wheel of a radius (R),
 the distance between the mass and the center of a wheel is (r),
 (if r == R we face Θ'' -> inf, Θ -> pi)
-initial angle of a wheel is Θ
+initial angle of a wheel is Θ, friction coefficient is (b)
 
 Motion equations:
 x = r * Θ
-Θ'' = (R * Θ'^2 + g) * r * sin(Θ) / (R^2 + 2 * r * R * cos(Θ) + r^2)
+Θ'' = ((R * Θ'^2 + g) * r * sin(Θ) - b * Θ') / (R^2 + 2 * r * R * cos(Θ) + r^2)
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,25 +18,28 @@ from ode_solver import solve, integrate_rk4
 r = 1.0
 R = 1.5
 g = 9.8
+b = 0.1  # friction
 
 
 def derivate(state, step, t, dt):
     dth, th = state
-    _dth = (R * dth ** 2 + g) * r * sin(th) / (R ** 2 + 2 * r * R * cos(th) + r ** 2)
+    _dth = ((R * dth ** 2 + g) * r * sin(th) - b * dth) / (R ** 2 + 2 * r * R * cos(th) + r ** 2)
     return [_dth, dth]
 
 
 if __name__ == "__main__":
-    times = np.linspace(0, 10, 1000)
+    times = np.linspace(0, 60, 2000)
     solution = solve([0.0, pi / 12], times, integrate_rk4, derivate)
     theta = solution[:, 1]
     wheel_x = theta * R
     mass_x = wheel_x + r * cos(theta - pi / 2)
     mass_y = R - r * sin(theta - pi / 2)
 
-    # w_line, = plt.plot(times, solution[:-1, 0], label="ω")
-    # th_line, = plt.plot(times, solution[:-1, 1], label="Θ")
-    # plt.legend([w_line, th_line], ['ω', 'Θ'])
+    w_line, = plt.plot(times, solution[:-1, 0], label="ω")
+    th_line, = plt.plot(times, solution[:-1, 1], label="Θ")
+    plt.legend([w_line, th_line], ['ω', 'Θ'])
+    plt.grid(True)
+    plt.show()
 
     fig = plt.figure()
     ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 12), ylim=(-0.5, 3.5))
@@ -63,10 +66,9 @@ if __name__ == "__main__":
 
     plt.show()
 
-
     # Set up formatting for the movie files
-    print("Writing video...")
-    Writer = animation.writers['ffmpeg']
-    writer = Writer(fps=15, metadata=dict(artist='Sergey Royz'), bitrate=1800)
-    ani.save(f"{__file__[:-3]}.mp4", writer=writer)
-    print("Done")
+    # print("Writing video...")
+    # Writer = animation.writers['ffmpeg']
+    # writer = Writer(fps=15, metadata=dict(artist='Sergey Royz'), bitrate=1800)
+    # ani.save(f"{__file__[:-3]}.mp4", writer=writer)
+    # print("Done")
