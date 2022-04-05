a184e3f1eaf0a20d61b1ced1b532ba5044248c96
Sergey Royz <zjor.se@gmail.com>
2022-04-05 12:06:10 +0200

    maze

--- python/problems/maze_manhatten.py
+++ python/problems/maze_manhatten.py
@@ -1,64 +1,92 @@
-def max_matrix(m):
-    result = m[0][0]
-    for row in m:
-        for col in row:
-            result = max(result, col)
-    return result
-
-
 def print_matrix(m, col_sep=' '):
     for row in m:
         print(col_sep.join(list(map(str, row))))
 
 
-def surround_matrix(m, x):
-    """
-    Adds values of "x" on the borders of the matrix "m",
-    expects "m" to be NxN
-    """
-    n = len(m)
-    result = [[x] * (n + 2)]
-    for row in m:
-        result.append([x] + row + [x])
-
-    result.append([x] * (n + 2))
-    return result
-
-
-def init_field(m):
-    result = []
-    for row in m:
-        result.append([])
-        for value in row:
-            result[-1].append([value, '' if value > 0 else 'x'])
-    return result
+def create_matrix(size, init='.'):
+    return [[init] * size for _ in range(size)]
 
 
 def traverse(m, size):
-    for row in range(1, size + 1):
-        for col in range(1, size + 1):
-            if row == 1 and col == 1:
+
+    if m[0][0] == 0 or m[size - 1][size - 1] == 0:
+        print("Impossible")
+        return
+
+    _m = []
+    for row in m:
+        _row = []
+        for col in row:
+            _row.append([col, None])
+        _m.append(_row)
+
+    for i in range(1, size):
+
+        if _m[0][i][0] > 0:
+            v, _ = _m[0][i - 1]
+            if v == 0:
+                _m[0][i] = [0, None]
+            else:
+                _m[0][i] = [_m[0][i][0] + v, (0, i - 1)]
+
+        if _m[i][0][0] > 0:
+            v, _ = _m[i - 1][0]
+            if v == 0:
+                _m[i][0] = [0, None]
+            else:
+                _m[i][0] = [_m[i][0][0] + v, (i - 1, 0)]
+
+    for r in range(1, size):
+        for c in range(1, size):
+            v, _ = _m[r][c]
+            if v == 0:
                 continue
 
-            current_cell = m[row][col]
-            left_cell = m[row - 1][col]
-            top_cell = m[row][col - 1]
+            left_v = _m[r][c - 1][0]
+            top_v = _m[r - 1][c][0]
 
-            if left_cell[1] == 'x' and top_cell[1] == 'x':
-                current_cell[1] = 'x'
-            elif left_cell[1] == 'x' or left_cell[0] > top_cell[0]:
-                current_cell[0] += top_cell[0]
-                top_cell[1] = 'D'
-            elif top_cell[1] == 'x' or top_cell[0] >= left_cell[0]:
-                current_cell[0] += left_cell[0]
-                left_cell[1] = 'R'
-            else:
-                raise Exception("Shouldn't get here")
-    return m
+            if left_v == 0 and top_v == 0:
+                continue
+
+            if left_v == 0:
+                _m[r][c] = [v + top_v, (r - 1, c)]
+            elif top_v == 0:
+                _m[r][c] = [v + left_v, (r, c - 1)]
+            elif left_v <= top_v and _m[r - 1][c][1] and _m[r][c - 1]:
+                _m[r][c] = [v + left_v, (r, c - 1)]
+            elif left_v > top_v and _m[r - 1][c][1] and _m[r][c - 1]:
+                _m[r][c] = [v + top_v, (r - 1, c)]
+
+    if _m[size - 1][size - 1][1]:
+        path = create_matrix(size, init='-')
+        r, c = size - 1, size - 1
+
+        while r != 0 or c != 0:
+            path[r][c] = '#'
+            r, c = _m[r][c][1]
+
+        path[r][c] = '#'
+        print_matrix(path, col_sep='')
+    else:
+        print("Impossible")
 
 
-data = [[1, 2], [3, 4]]
-data = surround_matrix(data, max_matrix(data) + 1)
-data = init_field(data)
-data = traverse(data, 2)
-print_matrix(data)
+def parse_input(_f):
+    return [list(map(int, list(row))) for row in _f.split()]
+
+
+data = """
+1112
+1001
+1101
+0110
+"""
+
+N = int(input())
+
+data = []
+for i in range(N):
+    data.append(input())
+
+data = parse_input('\n'.join(data))
+traverse(data, N)
