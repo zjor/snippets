45401adc7dfcceafe3f2f0398b2a33fde8506f0f
Sergey Royz <zjor.se@gmail.com>
2023-08-01 12:19:52 +0200

    pytetris: wip

--- python/pytetris/src/shapes.py
+++ python/pytetris/src/shapes.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 TEMPLATES = [
     [
         [1, 1],
@@ -31,29 +33,66 @@ TEMPLATES = [
 ]
 
 
+class Move(Enum):
+    LEFT = 1
+    RIGHT = 2
+    ROT_CW = 3
+    ROT_CCW = 4
+    UP = 5
+    DOWN = 6
+
+
 class Shape:
     def __init__(self, template: list[list[int]], rotation: int):
         self.template: list[list[int]] = template
         self.rotation: int = rotation
-        self.position: list[int] = [0, 0]
+        self.x = 0
+        self.y = 0
+        self.w = len(template[0])
+        self.h = len(template)
 
-    def _rotate(self) -> list[list[int]]:
+    def rotate(self) -> None:
         # TODO: impl rotation
-        return self.template
+        self.w = len(self.template[0])
+        self.h = len(self.template)
 
-    def advance(self):
-        self.position[1] += 1
+    def move(self, m: Move):
+        if m == Move.LEFT:
+            self.x -= 1
+        elif m == Move.RIGHT:
+            self.x += 1
+        elif m == Move.UP:
+            self.y -= 1
+        elif m == Move.DOWN:
+            self.y += 1
+        elif m == Move.ROT_CW:
+            pass
+        elif m == Move.ROT_CCW:
+            pass
+        else:
+            raise ValueError(f"Unsupported move: {m}")
+
+    def is_within_bounds(self, width: int, height: int) -> bool:
+        x_offset = width // 2 - 1
+        if self.x + x_offset < 0 or self.y < 0:
+            return False
+        elif self.x + x_offset + self.w > width or self.y + self.h > height:
+            return False
+        else:
+            return True
 
     def render(self, width: int, height: int) -> list[list[int]]:
+        if not self.is_within_bounds(width, height):
+            raise ValueError("Shape is outside the field")
+
         m = []
         for i in range(height):
             m.append([0] * width)
 
-        fig = self._rotate()
-        x, y = self.position
+        x, y = self.x, self.y
         x += width // 2 - 1
 
-        for i, row in enumerate(fig):
-            for j, cell in enumerate(fig[i]):
+        for i, row in enumerate(self.template):
+            for j, cell in enumerate(self.template[i]):
                 m[y + i][x + j] = cell
         return m
--- python/pytetris/src/tetris.py
+++ python/pytetris/src/tetris.py
@@ -1,5 +1,6 @@
 import random
 
+from textual import events
 from textual.app import App, ComposeResult
 from textual.widgets import Static
 
@@ -13,15 +14,38 @@ class Snowflakes(Static):
         self.width = width
         self.height = height
         self.shape = shapes.Shape(template=shapes.TEMPLATES[0], rotation=0)
+        self.state: list[list[int]] = []
+        self.init_state()
+
+    def init_state(self):
+        self.state = []
+        for i in range(self.height):
+            self.state.append([0] * self.width)
+
+    def merge_state(self) -> (bool, list[list[int]] | None):
+        new_state: list[list[int]] = []
+        r = self.shape.render(self.width, self.height)
+        for (i, row) in enumerate(self.state):
+            new_state.append([])
+            for (j, cell) in enumerate(row):
+                new_state[-1].append(1 if cell == 1 or r[i][j] == 1 else 0)
+                if cell == 1 and r[i][j] == 1:
+                    return False, None
+        return True, new_state
 
     def on_mount(self):
         self.set_interval(0.5, self.next_frame)
 
     def next_frame(self):
-        m = self.shape.render(self.width, self.height)
-        data = '\n'.join([''.join('  ' if cell == 0 else '[]' for cell in row) for row in m])
+        success, new_state = self.merge_state()
+        if success:
+            self.shape.move(shapes.Move.DOWN)
+        else:
+            self.shape.move(shapes.Move.UP)
+            _, new_state = self.merge_state()
+            self.state = new_state
+        data = '\n'.join([''.join('  ' if cell == 0 else '[]' for cell in row) for row in new_state])
         self.update(data)
-        self.shape.advance()
 
 
 class TetrisApp(App):
@@ -33,6 +57,13 @@ class TetrisApp(App):
 
     def compose(self) -> ComposeResult:
         yield self.field
+
+    def on_key(self, event: events.Key) -> None:
+        if event.key == 'a':
+            self.field.shape.move(shapes.Move.LEFT)
+        elif event.key == 'd':
+            self.field.shape.move(shapes.Move.RIGHT)
+        self.field.next_frame()
 
 
 def main():
