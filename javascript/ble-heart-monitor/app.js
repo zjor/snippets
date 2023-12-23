5f32d3b65c7f0f079ec4bdb5522855aee157ba42
Sergey Royz <zjor.se@gmail.com>
2023-12-24 00:23:21 +0100

    HRM deployed

--- javascript/ble-heart-monitor/app.js
+++ javascript/ble-heart-monitor/app.js
@@ -3,43 +3,59 @@ BATTERY_SERVICE = 0x180f
 
 const log = console.log
 
+const initMessage = document.querySelector('#init-message')
+const heartContainer = document.querySelector('#heart-container')
+const pulseDiv = document.querySelector('#pulse')
+const heartDiv = document.querySelector('.heart')
+
 async function main() {
   log('Connecting...')
+  initMessage.innerText = "Connecting..."
   const ble = navigator.bluetooth
   if (!await ble.getAvailability()) {
     log('BLE is not available')
+    initMessage.innerText = "BLE is not available"
     return
   }
 
-  const device = await ble.requestDevice({
-    // acceptAllDevices: true
-    filters: [{
-      services: [0x180d]
-    }]
-  })
-  const {id, name} = device
-  log(`Paired to (${name}; ID: ${id})`)
+  try {
+    const device = await ble.requestDevice({
+      filters: [{
+        services: [0x180d]
+      }]
+    })
 
-  const server = await device.gatt.connect()
-  log('Connected', server)
+    const {id, name} = device
+    log(`Paired to (${name}; ID: ${id})`)
 
-  const heartRateService = await server.getPrimaryService('heart_rate')
-  log('HR service', heartRateService)
+    const server = await device.gatt.connect()
+    log('Connected', server)
 
-  const hrCharacteristic = await heartRateService.getCharacteristic('00002a37-0000-1000-8000-00805f9b34fb')
-  log('Ch connected', hrCharacteristic)
-  hrCharacteristic.oncharacteristicvaluechanged = (event) => {
-    const data = event.target.value
-    log(data.getInt8(0, true))
-    log(data.getInt8(1, true))
-    log(data.getInt8(2, true))
-    log(data.getInt8(3, true))
-  }
-  hrCharacteristic.startNotifications()
-  log('Descriptor: ', await hrCharacteristic.getDescriptors())
-  // log('Value: ', await hrCharacteristic.readValue())
+    const heartRateService = await server.getPrimaryService('heart_rate')
+    log('HR service', heartRateService)
+
+    const hrCharacteristic = await heartRateService.getCharacteristic('00002a37-0000-1000-8000-00805f9b34fb')
+    log('Ch connected', hrCharacteristic)
+    hrCharacteristic.oncharacteristicvaluechanged = (event) => {
+      const data = event.target.value
+      log(data)
+      const pulse = data.getInt8(1)
+      pulseDiv.innerText = pulse
+
+      const duration = 60 / pulse
+      heartDiv.style.animation = `pulse ${duration}s infinite`
+    }
+    hrCharacteristic.startNotifications()
+    log('Descriptor: ', await hrCharacteristic.getDescriptors())
+    initMessage.style.display = 'none'
+    heartContainer.style.display = 'flex'
 
 // characteristing UUID: 00002902-0000-1000-8000-00805f9b34fb
+  } catch (e) {
+    initMessage.innerText = `Failed to connect: ${e}`
+  }
 }
 
 window.addEventListener('click', main)
+
+// deployed at: ble-heart-monitor.surge.sh
