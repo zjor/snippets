98f930bb1e0262db3ccfa921fdfbc80b3e8f93f6
Sergey Royz <zjor.se@gmail.com>
2024-02-04 22:56:54 +0100

    pulse-animation proto

--- javascript/sketches/pulse-animation/sketch.js
+++ javascript/sketches/pulse-animation/sketch.js
@@ -1,5 +1,7 @@
 const canvasSketch = require('canvas-sketch')
 
+const M_2PI = Math.PI * 2
+
 const [W, H] = [1080, 1080]
 
 const settings = {
@@ -15,6 +17,68 @@ function getAmp(t, tau, period) {
     return (period - t) / (period - tau)
   }
 }
+
+const PulseRing = ({radiusFunc, pointsCount, strokeStyle, lineWidth}) => {
+  return {
+    draw(ctx, time) {
+      const points = []
+      for (let i = 0; i < pointsCount; i++) {
+        const r = radiusFunc(i, pointsCount, time)
+        points.push({
+          x: W / 2 + r * Math.cos(i / pointsCount * M_2PI),
+          y: H / 2 + r * Math.sin(i / pointsCount * M_2PI)
+        })
+      }
+
+      ctx.beginPath()
+      ctx.moveTo(points[0].x, points[0].y)
+      for (let i = 1; i < pointsCount; i++) {
+        ctx.lineTo(points[i].x, points[i].y)
+      }
+      ctx.lineTo(points[0].x, points[0].y)
+      ctx.strokeStyle = strokeStyle
+      ctx.lineWidth = lineWidth
+      ctx.stroke()
+    }
+  }
+}
+
+const bpm = 67
+
+const ring1 = PulseRing({
+  radiusFunc: (i, n, time) => {
+    const a = -getAmp(time, 0.4, 60/bpm)
+    return 200 + a * (18 * Math.sin(i / n * 6 * Math.PI - 7 * time) +
+      12 * Math.sin(i / n * 12 * Math.PI + 4 * time))
+  },
+  pointsCount: 100,
+  strokeStyle: 'rgba(0, 0, 0, 0.2)',
+  lineWidth: 12
+})
+
+const ring2 = PulseRing({
+  radiusFunc: (i, n, time) => {
+    const a = -getAmp(time, 0.3, 60/bpm)
+    return 200 + a * (12 * Math.sin(i / n * 14 * Math.PI + time) +
+      18 * Math.sin(i / n * 18 * Math.PI - 3 * time))
+  },
+  pointsCount: 100,
+  strokeStyle: 'rgba(0, 0, 0, 1)',
+  lineWidth: 2
+})
+
+const ring3 = PulseRing({
+  radiusFunc: (i, n, time) => {
+    const a = -getAmp(time, 0.2, 60/bpm)
+    return 200 + a * (31 * Math.sin(i / n * 8 * Math.PI + time) +
+      21 * Math.sin(i / n * 12 * Math.PI + time * 2) +
+      8 * Math.sin(i / n * 24 * Math.PI + time * 3))
+  },
+  pointsCount: 100,
+  strokeStyle: 'rgba(0, 0, 0, 0.8)',
+  lineWidth: 6
+})
+
 
 let time = Date.now()
 
@@ -32,61 +96,20 @@ const sketch = ({canvas}) => {
     const now = Date.now() / 1000
     time = now
 
-    const a = - getAmp(time, 0.2, 1)
-    const a1 = - getAmp(time, 0.3, 1)
-    const a2 = - getAmp(time, 0.4, 1)
-
     ctx.fillStyle = '#fff'
     ctx.fillRect(0, 0, W, H)
 
-    const points = []
-    const pointsA = []
-    const pointsB = []
+    ring1.draw(ctx, time)
+    ring2.draw(ctx, time)
+    ring3.draw(ctx, time)
 
-    for (let i = 0; i < n; i++) {
-      const y = 100 * Math.sin(i / n * 7/3 * Math.PI + time) +
-        75 * Math.sin(i / n * 11/3 * Math.PI + time * 2) +
-        35 * Math.sin(i / n * 27/5 * Math.PI + time * 3);
-      points.push([xs[i], a * y])
-      pointsA.push({
-        x: xs[i],
-        y: a1 * (25 * Math.sin(i / n * 2 * Math.PI + time) +
-          15 * Math.sin(i / n * 6 * Math.PI + 2 * time))
-      })
-      pointsB.push({
-        x: xs[i],
-        y: a2 * (50 * Math.sin(i / n * 4 * Math.PI + 3 * time) +
-          25 * Math.sin(i / n * 12 * Math.PI + 4 * time))
-      })
+    ctx.fillStyle = '#9f0000'
+    ctx.font = "normal 60px monospace"
+    let m = ctx.measureText(`${bpm}`)
+    ctx.fillText(`${bpm}`, W / 2 - m.width / 2, H / 2)
 
-    }
-
-    ctx.beginPath()
-    ctx.moveTo(points[0][0], H / 2 + points[0][1])
-    for (let i = 1; i < n; i++) {
-      ctx.lineTo(points[i][0], H / 2 + points[i][1])
-    }
-    ctx.lineWidth = 6
-    ctx.stroke()
-
-    ctx.beginPath()
-    ctx.moveTo(pointsA[0].x, H / 2 + pointsA[0].y)
-    for (let i = 1; i < n; i++) {
-      ctx.lineTo(pointsA[i].x, H / 2 + pointsA[i].y)
-    }
-    ctx.strokeStyle = 'rgba(0, 0, 0, 0.6)'
-    ctx.lineWidth = 3
-    ctx.stroke()
-
-    ctx.beginPath()
-    ctx.moveTo(pointsB[0].x, H / 2 + pointsB[0].y)
-    for (let i = 1; i < n; i++) {
-      ctx.lineTo(pointsB[i].x, H / 2 + pointsB[i].y)
-    }
-    ctx.strokeStyle = 'rgba(0, 0, 0, 0.2)'
-    ctx.lineWidth = 12
-    ctx.stroke()
-
+    m = ctx.measureText("BPM")
+    ctx.fillText("BPM", W / 2 - m.width / 2, H / 2 + 60)
 
   }
 }
