080c1376e3c2675f55cd9fd5110700ac7fa4c332
Sergey Royz <zjor.se@gmail.com>
2024-03-30 21:37:26 +0100

    translator & split-screen

--- go/translator/anki/anki.go
+++ go/translator/anki/anki.go
@@ -6,16 +6,71 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/replicate/replicate-go"
 	"github.com/sashabaranov/go-openai"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
+
+const TARGET_IMAGE_WIDTH = 1080
+const TARGET_IMAGE_HEIGHT = 1080
 
 type Translation struct {
 	Word        string `json:"word"`
 	Translation string `json:"translation"`
 	Example     string `json:"example"`
 	ImagePrompt string `json:"image_prompt"`
+}
+
+func DownloadFile(url, destination string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	outFile, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GenerateImage(prompt string) string {
+	ctx := context.Background()
+	r8, err := replicate.NewClient(replicate.WithTokenFromEnv())
+	if err != nil {
+		log.Fatal(err)
+	}
+	version := "stability-ai/sdxl:da77bc59ee60423279fd632efb4795ab731d9e3ca9705ef3341091fb989b7eaf"
+	input := replicate.PredictionInput{
+		"prompt": prompt,
+		"width":  936,
+		"height": 752,
+	}
+
+	webhook := replicate.Webhook{
+		URL:    "https://example.com/webhook",
+		Events: []replicate.WebhookEventType{"start", "completed"},
+	}
+	output, err := r8.Run(ctx, version, input, &webhook)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Image generated: %v", output)
+	return output.([]interface{})[0].(string)
 }
 
 func Translate(what, from, to string) (*Translation, error) {
@@ -64,10 +119,58 @@ Output in JSON format with the following keys: "word", "translation", "example",
 	return t, nil
 }
 
-func main() {
-	t, err := Translate("eloquent", "en", "ru")
+func fill(img *image.RGBA, color color.RGBA) {
+	for y := 0; y < img.Bounds().Dy(); y++ {
+		for x := 0; x < img.Bounds().Dx(); x++ {
+			img.Set(x, y, color)
+		}
+	}
+}
+
+func BuildFlashCard(imageFilename, outImageFilename string) error {
+	imageFile, err := os.Open(imageFilename)
+	if err != nil {
+		return err
+	}
+	img, err := png.Decode(imageFile)
+	if err != nil {
+		return nil
+	}
+
+	targetImage := image.NewRGBA(image.Rect(0, 0, TARGET_IMAGE_WIDTH, TARGET_IMAGE_HEIGHT))
+	fill(targetImage, color.RGBA{165, 201, 89, 255})
+	imgOffset := image.Point{
+		X: (TARGET_IMAGE_WIDTH - img.Bounds().Dx()) / 2,
+		Y: (TARGET_IMAGE_HEIGHT - img.Bounds().Dy()) / 2,
+	}
+	draw.Draw(targetImage, img.Bounds().Add(imgOffset), img, image.Point{}, draw.Over)
+	outImageFile, err := os.Create(outImageFilename)
+	if err != nil {
+		return err
+	}
+	defer outImageFile.Close()
+	if err := png.Encode(outImageFile, targetImage); err != nil {
+		return err
+	}
+	return nil
+}
+
+func RunPipeline(word string) {
+	t, err := Translate(word, "en", "ru")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%+v\n", t)
+	GenerateImage(t.ImagePrompt)
+}
+
+func main() {
+	//RunPipeline("eloquent")
+	//url := "https://replicate.delivery/pbxt/4aUmb2ujB1JffkPSa90bRw9ZGTtOZGe9BteiS95DPt7vBIWKB/out-0.png"
+	//err := DownloadFile(url, ".images/image.png")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	BuildFlashCard(".images/image.png", ".images/out_image.png")
+
 }
--- go/translator/go.mod
+++ go/translator/go.mod
@@ -5,8 +5,10 @@ go 1.21.5
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/replicate/replicate-go v0.18.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sashabaranov/go-openai v1.20.4 // indirect
 	github.com/urfave/cli/v2 v2.27.1 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 )
