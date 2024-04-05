2a436a5812b40d18b02cd1b3f488a45162693e50
Sergey Royz <zjor.se@gmail.com>
2024-04-05 13:14:10 +0200

    highlight splittable zone

--- javascript/split-screen/src/components/Splittable/Splittable.jsx
+++ javascript/split-screen/src/components/Splittable/Splittable.jsx
@@ -1,49 +1,36 @@
 import styles from './Splittable.module.css'
 
 function Splittable({depth, content, color, onSplit}) {
-    // const onMouseMove = (e) => {
-    //     const n = e.nativeEvent
-    //     const t = e.target
-    //     const [dx, dy] = [
-    //         Math.floor(t.offsetWidth / 3),
-    //         Math.floor(t.offsetHeight / 3)
-    //     ]
-    //     const [bx, by] = [
-    //         Math.floor(n.offsetX / dx),
-    //         Math.floor(n.offsetY / dy)
-    //     ]
-    //     console.log(bx, by)
-    // }
-
-    const onClick = (e) => {
-        const n = e.nativeEvent
-        const t = e.target
-        const [dx, dy] = [
-            Math.floor(t.offsetWidth / 3),
-            Math.floor(t.offsetHeight / 3)
-        ]
-        const [bx, by] = [
-            Math.floor(n.offsetX / dx),
-            Math.floor(n.offsetY / dy)
-        ]
-        let direction = undefined
-        if ((bx == 0 || bx == 2) && by == 1) {
-            direction = 'vertical'
-        } else if ((by == 0 || by == 2) && bx == 1) {
-            direction = 'horizontal'
-        }
-
-        onSplit(direction)
-    }
     return (
         <div
             className={styles.wrapper}
-            style={{backgroundColor: color}}
-            // onMouseMove={onMouseMove}
-            onClick={onClick}>
+            style={{backgroundColor: color}}>
             <div className={styles.content}>
                 <div className={styles.depth}>{depth}</div>
                 {content}
+                <div className={styles.grid}>
+                    <div></div>
+                    <div
+                        className={styles.clickable}
+                        onClick={() => onSplit('horizontal')}>
+                    </div>
+                    <div></div>
+                    <div
+                        className={styles.clickable}
+                        onClick={() => onSplit('vertical')}>
+                    </div>
+                    <div></div>
+                    <div
+                        className={styles.clickable}
+                        onClick={() => onSplit('vertical')}>
+                    </div>
+                    <div></div>
+                    <div
+                        className={styles.clickable}
+                        onClick={() => onSplit('horizontal')}>
+                    </div>
+                    <div></div>
+                </div>
             </div>
         </div>
     )
