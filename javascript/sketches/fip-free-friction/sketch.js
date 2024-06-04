68601208f7ea5091bb207ee05c8c810c877bdd28
Sergey Royz <zjor.se@gmail.com>
2024-06-05 00:31:13 +0200

    FIP: impl

--- javascript/sketches/fip-free-friction/sketch.js
+++ javascript/sketches/fip-free-friction/sketch.js
@@ -20,16 +20,50 @@ let paused = true
 
 const g = 9.81
 
-const m1 = 0.5
+const m1 = 0.9
 const m2 = 1.0
+const r = 0.4
 const l = 1.0
-const J = 0.25
-const b = 0.01
+const J = m2 * r ** 2
+const b = 100
 
-let th = pi / 6
+const th0 = pi / 6
+let th = th0
 let dth = .0
 let phi = .0
 let dphi = .0
+
+/**
+ *
+ * @param c {CanvasRenderingContext2D}
+ * @param x {Number}
+ * @param y {Number}
+ * @param scale {Number}
+ */
+function renderWheel(c, x, y, scale) {
+    const _r = scale * r
+    c.strokeStyle = BLUE
+    c.lineWidth = 4.0
+    c.beginPath()
+    c.ellipse(x, y, _r, _r, 0, 0, 2 * pi)
+    c.stroke()
+
+    const vs = []
+    for (let i = 0; i < 4; i++) {
+        vs.push([
+            x + _r * cos(i * pi / 2 + phi - th0 / 2),
+            y + _r * sin(i * pi / 2 + phi - th0 / 2)
+        ])
+    }
+
+    for (let v of vs) {
+        c.beginPath()
+        c.moveTo(x, y)
+        c.lineTo(...v)
+        c.stroke()
+    }
+
+}
 
 /**
  *
@@ -38,10 +72,33 @@ let dphi = .0
  * @param height {Number}
  */
 function render(c, width, height) {
-    const origin = [width / 2, height / 3]
+    const origin = [width / 2, height / 2]
     const scale = width / 4
 
-    //TODO: impl
+    const [x, y] = [
+        l * sin(th) * scale + origin[0],
+        -l * cos(th) * scale + origin[1]
+    ]
+
+    c.strokeStyle = BLUE
+    c.lineWidth = 2.0
+    c.beginPath()
+    c.moveTo(...origin)
+    c.lineTo(x, y)
+    c.stroke()
+
+    c.strokeStyle = BLUE
+    c.lineWidth = 4.0
+    c.beginPath()
+    c.moveTo(origin[0] - scale, origin[1])
+    c.lineTo(origin[0] + scale, origin[1])
+    c.stroke()
+
+    c.fillStyle = PINK
+    c.ellipse(...origin, 10, 10, 0, 0, 2 * pi)
+    c.fill()
+
+    renderWheel(c, x, y, scale)
 }
 
 /**
@@ -52,7 +109,10 @@ function render(c, width, height) {
  * @returns {Array<Number>}
  */
 function derive(state, t, dt) {
-    //TODO: impl
+    const [_th, _dth, _phi, _dphi] = state
+    ddth = (b * _dphi - (0.5 * m1 + m2) * l * g * sin(-_th)) / (m1 * l ** 2 / 3 + m2 * l ** 2 + J)
+    ddphi = -b * _dphi / J
+    return [_dth, ddth, _dphi, ddphi]
 }
 
 /**
