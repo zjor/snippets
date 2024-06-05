86dc3cfb81de45ccb611ef90f19e8b5fa7002810
Sergey Royz <zjor.se@gmail.com>
2024-06-05 23:29:21 +0200

    added centroid markers

--- javascript/sketches/fip-free-friction/sketch.js
+++ javascript/sketches/fip-free-friction/sketch.js
@@ -7,7 +7,7 @@ const settings = {
     dimensions: [1080, 1080],
     animate: true
 }
-
+const BLACK = '#000'
 const GREEN = '#01DC03' // rgb(1, 220, 3)
 const DARK_GREEN = '#017A02'
 const BLUE = '#12B8FF' // rgb(18, 184, 255)
@@ -21,17 +21,60 @@ let paused = true
 const g = 9.81
 
 const m1 = 0.9
-const m2 = 1.0
+const m2 = 2.0
 const r = 0.4
-const l = 1.0
+const l = 1.25
 const J = m2 * r ** 2
-const b = 100
+const b = 0.9
 
 const th0 = pi / 6
 let th = th0
 let dth = .0
 let phi = .0
 let dphi = .0
+
+/**
+ * Renders a circle with a black and yellow quadrants
+ * @param c {CanvasRenderingContext2D}
+ * @param x {Number}
+ * @param y {Number}
+ * @param r {Number}
+ * @param phi {Number}
+ */
+function renderCentroid(c, x, y, r, phi) {
+    c.fillStyle = YELLOW
+
+    c.beginPath()
+    c.moveTo(x, y)
+    c.arc(x, y, r, phi, pi / 2 + phi, false)
+    c.closePath()
+    c.fill()
+
+    c.beginPath()
+    c.moveTo(x, y)
+    c.arc(x, y, r, pi + phi, pi + pi / 2 + phi, false)
+    c.closePath()
+    c.fill()
+
+    c.fillStyle = BLACK
+    c.beginPath()
+    c.moveTo(x, y)
+    c.arc(x, y, r, pi / 2 + phi, pi + phi, false)
+    c.closePath()
+    c.fill()
+
+    c.beginPath()
+    c.moveTo(x, y)
+    c.arc(x, y, r, pi + pi / 2 + phi, 2 * pi + phi, false)
+    c.closePath()
+    c.fill()
+
+    c.strokeStyle = YELLOW
+    c.lineWidth = 1
+    c.beginPath()
+    c.ellipse(x, y, r, r, 0, 0, 2 * pi)
+    c.stroke()
+}
 
 /**
  *
@@ -43,7 +86,7 @@ let dphi = .0
 function renderWheel(c, x, y, scale) {
     const _r = scale * r
     c.strokeStyle = BLUE
-    c.lineWidth = 4.0
+    c.lineWidth = 6.0
     c.beginPath()
     c.ellipse(x, y, _r, _r, 0, 0, 2 * pi)
     c.stroke()
@@ -56,12 +99,15 @@ function renderWheel(c, x, y, scale) {
         ])
     }
 
+    c.lineWidth = 3.0
     for (let v of vs) {
         c.beginPath()
         c.moveTo(x, y)
         c.lineTo(...v)
         c.stroke()
     }
+
+    renderCentroid(c, x, y, 20, phi - th0 / 2)
 
 }
 
@@ -81,7 +127,7 @@ function render(c, width, height) {
     ]
 
     c.strokeStyle = BLUE
-    c.lineWidth = 2.0
+    c.lineWidth = 6.0
     c.beginPath()
     c.moveTo(...origin)
     c.lineTo(x, y)
@@ -94,9 +140,7 @@ function render(c, width, height) {
     c.lineTo(origin[0] + scale, origin[1])
     c.stroke()
 
-    c.fillStyle = PINK
-    c.ellipse(...origin, 10, 10, 0, 0, 2 * pi)
-    c.fill()
+    renderCentroid(c, ...origin, 20, th)
 
     renderWheel(c, x, y, scale)
 }
@@ -110,8 +154,9 @@ function render(c, width, height) {
  */
 function derive(state, t, dt) {
     const [_th, _dth, _phi, _dphi] = state
-    ddth = (b * _dphi - (0.5 * m1 + m2) * l * g * sin(-_th)) / (m1 * l ** 2 / 3 + m2 * l ** 2 + J)
-    ddphi = -b * _dphi / J
+    const friction = -b * (_dphi - _dth)
+    ddth = (-friction - (0.5 * m1 + m2) * l * g * sin(-_th)) / (m1 * l ** 2 / 3 + m2 * l ** 2 + J)
+    ddphi = friction / J
     return [_dth, ddth, _dphi, ddphi]
 }
 
